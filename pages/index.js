e3c71cbb49ecf601981699dd78db966ba1e1cb83
sofiaune <87231622+sofiaune@users.noreply.github.com>
2021-07-10 05:52:30 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,34 +19,41 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+		<div class="clearfix"></div>
+							<div class="posts-image posts-image-single">
+					<div class="posts-image-content">
+						<a href="https://selectra.info/sites/default/files/styles/article_hero/public/ma-french-bank-gros-logo.jpg?itok=awzJG9UK" target="_blank">
+							<img class="img-fluid" src="https://selectra.info/sites/default/files/styles/article_hero/public/ma-french-bank-gros-logo.jpg?itok=awzJG9UK" alt="Ma French Bank La Banque Postale En Ligne Offres Tarifs" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh_l3eQ5xwiPy07kGEXjmjgmBKBRB7H2mRxCGhv1tFWg5c_mWT';"></a>
+						<p>Ma French Bank La Banque Postale En Ligne Offres Tarifs</p> 
+						
+					</div>
+				</div>
+							<div class="posts-image posts-image-single">
+					<div class="posts-image-content">
+						<a href="https://www.expertpublic.fr/wp-content/uploads/2019/08/arreter-prelevement-auto-banque-postale.jpg" target="_blank">
+							<img class="img-fluid" src="https://www.expertpublic.fr/wp-content/uploads/2019/08/arreter-prelevement-auto-banque-postale.jpg" alt="Comment Arreter Un Prelevement Automatique A La Banque Postale" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh_l3eQ5xwiPy07kGEXjmjgmBKBRB7H2mRxCGhv1tFWg5c_mWT';"></a>
+						<p>Comment Arreter Un Prelevement Automatique A La Banque Postale</p> 
+						
+					</div>
+				</div>
+							<div class="posts-image posts-image-single">
+					<div class="posts-image-content">
+						<a href="https://ilbi.org/wp-content/uploads/2019/09/comment-activer-carte-prepayee-banque-postale-990x659.jpg" target="_blank">
+							<img class="img-fluid" src="https://ilbi.org/wp-content/uploads/2019/09/comment-activer-carte-prepayee-banque-postale-990x659.jpg" alt="Comment Activer La Carte Prepayee De La Banque Postale Ilbi Org" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh_l3eQ5xwiPy07kGEXjmjgmBKBRB7H2mRxCGhv1tFWg5c_mWT';"></a>
+						<p>Comment Activer La Carte Prepayee De La Banque Postale Ilbi Org</p> 
+						
+					</div>
+				</div>
+							<div class="posts-image posts-image-single">
+					<div class="posts-image-content">
+						<a href="https://www.associationeconomienumerique.fr/wp-content/uploads/2019/11/page-accueil-espace-client-banque-postale.jpg" target="_blank">
+							<img class="img-fluid" src="https://www.associationeconomienumerique.fr/wp-content/uploads/2019/11/page-accueil-espace-client-banque-postale.jpg" alt="Consulter Ses Comptes A La Banque Postale Les Etapes" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh_l3eQ5xwiPy07kGEXjmjgmBKBRB7H2mRxCGhv1tFWg5c_mWT';"></a>
+						<p>Consulter Ses Comptes A La Banque Postale Les Etapes</p> 
+						
+					</div>
+				</div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
