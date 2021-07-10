f8d1985eee49679e35bcfe5b039aed964808848b
sofiaune <87231622+sofiaune@users.noreply.github.com>
2021-07-10 05:50:06 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,211 +1,65 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
-<!doctype html>
-<html lang="en">
-<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">	
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-	<style type="text/css">
-body,html{font-family:"Georgia",Times,Times New Roman,serif;font-size:18px;font-weight:600;background:#f2f2f2}img{max-width:100%}.header{padding-bottom:15px;margin-bottom:15px}.container{max-width:1024px}.navbar{margin-bottom:25px;color:#f1f2f3;border-bottom:1px solid #e1e2e3}.navbar .navbar-brand{margin:0 20px 0 0;font-size:28px;padding:0;line-height:24px}.row.content,.row.footer,.row.header{widthx:auto;max-widthx:100%}.row.footer{padding:30px 0;background:0 0}.content .col-sm-12{padding:0}.content .col-md-9s{margin-right:-25px}.posts-image{width:25%;display:block;float:left}.posts-image-content{width:auto;margin:0 15px 35px 0;position:relative}.posts-image:nth-child(5n) .posts-image-content{margin:0 0 35px!important}.posts-image img{width:100%;height:150px;object-fit:cover;object-position:center;margin-bottom:10px;-webkit-transition:.6s opacity;transition:.6s opacity}.posts-image:hover img{opacity:.8}.posts-image:hover h2{background:rgba(0,0,0,.7)}.posts-image h2{z-index:2;position:absolute;font-size:14px;bottom:2px;left:0;right:0;padding:5px;text-align:center;-webkit-transition:.6s opacity;transition:.6s background}.posts-image h2 a{color:#fff}.posts-image-big{display:block;width:100%}.posts-image-big .posts-image-content{margin:0 0 10px!important}.posts-image-big img{height:380px}.posts-image-big h2{text-align:left;padding-left:0;position:relative;font-size:30px;line-height:36px}.posts-image-big h2 a{color:#111}.posts-image-big:hover h2{background:0 0}.posts-image-single .posts-image-content,.posts-image-single:nth-child(5n) .posts-image-content{margin:0 15px 15px 0!important}.posts-image-single p{font-size:14px;line-height:18px!important}.posts-images{clear:both}.list-group li{padding:0}.list-group li a{display:block;padding:8px}.widget{margin-bottom:20px}h3.widget-title{font-size:20px}a{color:#f42966;text-decoration:none}.footer{margin-top:21px;padding-top:13px;border-top:1px solid #eee}.footer a{margin:0 15px}.navi{margin:13px 0}.navi a{margin:5px 2px;font-size:95%}@media  only screen and (min-width:0px) and (max-width:991px){.container{width:auto;max-width:100%}.navbar{padding:5px 0}.navbar .container{width:100%;margin:0 15px}}@media  only screen and (min-width:0px) and (max-width:767px){.content .col-md-3{padding:15px}}@media  only screen and (min-width:481px) and (max-width:640px){.posts-image img{height:90px}.posts-image-big img{height:320px}.posts-image-single{width:33.3%}.posts-image-single:nth-child(3n) .posts-image-content{margin-right:0!important}}@media  only screen and (min-width:0px) and (max-width:480px){.posts-image img{height:80px}.posts-image-big img{height:240px}.posts-image-single{width:100%}.posts-image-single .posts-image-content{margin:0!important}.posts-image-single img{height:auto}}
-	</style>
-	<title>Comment Faire Opposition La Banque Postale</title>
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-	<meta name="google-site-verification" content="VR_8i8uCCwWRo8u3H7VG7ooU7HpFEDzOsQ5DAL4THY4" /></head>
-<body>
-	<main id="main">
-		<nav class="navbar navbar-expand-lg navbar-light bg-light ">
-			<div class="container">
-		  <a href="" class="navbar-brand">Comment faire</a>
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		  </button>
-		  
-		  
-		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-			
-			<li class="nav-item"><a class="nav-link" href="/index.html">Home</a></li>
-			<li class="nav-item"><a class="nav-link" href="/Contact.html">Contact</a></li>
-			<li class="nav-item"><a class="nav-link" href="/DMCA.html">DMCA</a></li>
-			<li class="nav-item"><a class="nav-link" href="/Privacy.html">Privacy</a></li>
-			<li class="nav-item"><a class="nav-link" href="/sitemap.xml">Sitemap</a></li>
-			
-			</div>
-		  </div>
-		</nav>
-		<div class="container">  
-			<div class="row content">
-				<div class="col-md-9">
-					<div class="col-sm-12">
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          1Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
 
-							<h1>Comment Faire Opposition La Banque Postale</h1>
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
 
-	
-	<div class="navi text-left">
-			</div>
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
 
-						
-					</div> 
-					<div class="col-sm-12">					
-							
-	
-		
-		<div class="clearfix"></div>
-							<div class="posts-image posts-image-single">
-					<div class="posts-image-content">
-						<a href="https://selectra.info/sites/default/files/styles/article_hero/public/ma-french-bank-gros-logo.jpg?itok=awzJG9UK" target="_blank">
-							<img class="img-fluid" src="https://selectra.info/sites/default/files/styles/article_hero/public/ma-french-bank-gros-logo.jpg?itok=awzJG9UK" alt="Ma French Bank La Banque Postale En Ligne Offres Tarifs" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh_l3eQ5xwiPy07kGEXjmjgmBKBRB7H2mRxCGhv1tFWg5c_mWT';"></a>
-						<p>Ma French Bank La Banque Postale En Ligne Offres Tarifs</p> 
-						
-					</div>
-				</div>
-							<div class="posts-image posts-image-single">
-					<div class="posts-image-content">
-						<a href="https://www.expertpublic.fr/wp-content/uploads/2019/08/arreter-prelevement-auto-banque-postale.jpg" target="_blank">
-							<img class="img-fluid" src="https://www.expertpublic.fr/wp-content/uploads/2019/08/arreter-prelevement-auto-banque-postale.jpg" alt="Comment Arreter Un Prelevement Automatique A La Banque Postale" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh_l3eQ5xwiPy07kGEXjmjgmBKBRB7H2mRxCGhv1tFWg5c_mWT';"></a>
-						<p>Comment Arreter Un Prelevement Automatique A La Banque Postale</p> 
-						
-					</div>
-				</div>
-							<div class="posts-image posts-image-single">
-					<div class="posts-image-content">
-						<a href="https://ilbi.org/wp-content/uploads/2019/09/comment-activer-carte-prepayee-banque-postale-990x659.jpg" target="_blank">
-							<img class="img-fluid" src="https://ilbi.org/wp-content/uploads/2019/09/comment-activer-carte-prepayee-banque-postale-990x659.jpg" alt="Comment Activer La Carte Prepayee De La Banque Postale Ilbi Org" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh_l3eQ5xwiPy07kGEXjmjgmBKBRB7H2mRxCGhv1tFWg5c_mWT';"></a>
-						<p>Comment Activer La Carte Prepayee De La Banque Postale Ilbi Org</p> 
-						
-					</div>
-				</div>
-							<div class="posts-image posts-image-single">
-					<div class="posts-image-content">
-						<a href="https://www.associationeconomienumerique.fr/wp-content/uploads/2019/11/page-accueil-espace-client-banque-postale.jpg" target="_blank">
-							<img class="img-fluid" src="https://www.associationeconomienumerique.fr/wp-content/uploads/2019/11/page-accueil-espace-client-banque-postale.jpg" alt="Consulter Ses Comptes A La Banque Postale Les Etapes" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh_l3eQ5xwiPy07kGEXjmjgmBKBRB7H2mRxCGhv1tFWg5c_mWT';"></a>
-						<p>Consulter Ses Comptes A La Banque Postale Les Etapes</p> 
-						
-					</div>
-				</div>
-						 
-		 
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
 
-		
-		<div class="clearfix"></div>
-							<div class="posts-image posts-image-single">
-					<div class="posts-image-content">
-						<a href="https://les-sav.fr/wp-content/uploads/2018/01/encart-tel-opp-banque-postale.jpg" target="_blank">
-							<img class="img-fluid" src="https://les-sav.fr/wp-content/uploads/2018/01/encart-tel-opp-banque-postale.jpg" alt="La Banque Postale Faire Opposition A Votre Carte Bancaire Les" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh_l3eQ5xwiPy07kGEXjmjgmBKBRB7H2mRxCGhv1tFWg5c_mWT';"></a>
-						<p>La Banque Postale Faire Opposition A Votre Carte Bancaire Les</p> 
-						
-					</div>
-				</div>
-							<div class="posts-image posts-image-single">
-					<div class="posts-image-content">
-						<a href="https://transferwise-blog.s3.amazonaws.com/frais-retrait-etranger-banque-postale.jpg" target="_blank">
-							<img class="img-fluid" src="https://transferwise-blog.s3.amazonaws.com/frais-retrait-etranger-banque-postale.jpg" alt="Quels Sont Les Frais De Retrait A L Etranger Avec La Banque" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh_l3eQ5xwiPy07kGEXjmjgmBKBRB7H2mRxCGhv1tFWg5c_mWT';"></a>
-						<p>Quels Sont Les Frais De Retrait A L Etranger Avec La Banque</p> 
-						
-					</div>
-				</div>
-							<div class="posts-image posts-image-single">
-					<div class="posts-image-content">
-						<a href="https://billetdebanque.panorabanques.com/wp-content/uploads/2016/05/La-Banque-Postale-a-10-ans.jpg" target="_blank">
-							<img class="img-fluid" src="https://billetdebanque.panorabanques.com/wp-content/uploads/2016/05/La-Banque-Postale-a-10-ans.jpg" alt="La Banque Postale Reinvente La Carte Bancaire Parametrable" onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh_l3eQ5xwiPy07kGEXjmjgmBKBRB7H2mRxCGhv1tFWg5c_mWT';"></a>
-						<p>La Banque Postale Reinvente La Carte Bancaire Parametrable</p> 
-						
-					</div>
-				</div>
-						 
-		 
-			<div class="posts-images"> 
-				<div class="text-left">
-					<h3></h3>
-					
-					<p class="text-center"><img src="https://www.egaliteetreconciliation.fr/local/cache-vignettes/L650xH1157/ClotureCPT-1flou-f33b1.jpg" style="margin-bottom: 8px;"></p>
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
 
-									</div> 
-			</div> 
-		 
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
 
-	 	
-	
-					</div>
-				</div> 
-				<div class="col-md-3">
-					<div class="col-sm-12 widget">
-						<h3 class="widget-title">Random Posts</h3>
-						<ul class="list-group">
-						 
-																	<li class="list-group-item"><a href="comment-faire-un-western-union-à-la-poste.html">Comment Faire Un Western Union à La Poste</a></li>
-																	<li class="list-group-item"><a href="comment-faire-appel-masqué-sur-samsung.html">Comment Faire Appel Masqué Sur Samsung</a></li>
-																	<li class="list-group-item"><a href="comment-faire-une-rupture-conventionnelle-en-arret-maladie.html">Comment Faire Une Rupture Conventionnelle En Arret Maladie</a></li>
-								 
-						 
-																	<li class="list-group-item"><a href="comment-faire-un-gateau-d-anniversaire-pour-chien.html">Comment Faire Un Gateau D Anniversaire Pour Chien</a></li>
-																	<li class="list-group-item"><a href="comment-faire-un-gateau-au-chocolat-sans-four.html">Comment Faire Un Gateau Au Chocolat Sans Four</a></li>
-																	<li class="list-group-item"><a href="comment-faire-des-sushi-sans-kit.html">Comment Faire Des Sushi Sans Kit</a></li>
-								 
-						 
-																	<li class="list-group-item"><a href="comment-faire-son-levain-sans-gluten.html">Comment Faire Son Levain Sans Gluten</a></li>
-																	<li class="list-group-item"><a href="comment-faire-un-cv-étudiant-sans-expérience.html">Comment Faire Un Cv étudiant Sans Expérience</a></li>
-																	<li class="list-group-item"><a href="comment-faire-une-pâte-à-pizza-maison-facile.html">Comment Faire Une Pâte à Pizza Maison Facile</a></li>
-								 
-						 
-																	<li class="list-group-item"><a href="origami-comment-faire-un-cadeau-pour-sa-maman-facile.html">Origami Comment Faire Un Cadeau Pour Sa Maman Facile</a></li>
-								 
-						 
-						</ul>
-					</div>
-					<div class="col-sm-12 widget">
-						
-					</div>
-				</div> 
-			</div>
-			<div class="row footer">
-				<div class="col-md-12 text-center">
-
-	
-	
-				
-		
-
-
-				</div>
-			</div>
-		</div>
-		<!-- <style>
-	#topbar{  background:#ddd;  width:100%;  text-align:center;  color:#333;  padding:5px;  overflow:hidden;  height:89px;  z-index:1000;  font-family:Georgia;  font-size:17px;  line-height:30px;  position:fixed;  bottom:0;  left:0;  border-top:3px solid #26ADE4;  box-shadow:0 1px 5px rgba(0,0,0,.7);
-	}
-
-	#topbar a{-webkit-box-shadow:rgba(0,0,0,0.278431) 1px 1px 3px; background:#26ADE4;  border-bottom-left-radius:4px;  border-bottom-right-radius:4px;  border-top-left-radius:4px;  border-top-right-radius:4px;  border:none;  box-shadow:rgba(0,0,0,0.278431) 1px 1px 3px;  color:white;  cursor:pointer;  font-size:0.95em;  margin:0px 0px 0px 7px;  outline:none;  padding:5px 13px 5px;  position:relative;  text-decoration:initial;
-		font-size:19px;
-	}
-
-	#topbar a:hover{  cursor:pointer;background:#444}
-
-	#topbar a:active{  top:1px}
-
-</style>
-
-<div id='topbar'>
-	<p>Write an awesome ad copy here. 
-	  	<br>
-		<a href="#your-offer-url" target="_blank">CTA Link &rarr;</a>
-	</p>
-</div> -->		<!-- Histats.com  START  (aync)-->
-<script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4416148,4,0,0,0,00010000']);
-_Hasync.push(['Histats.fasi', '1']);
-_Hasync.push(['Histats.track_hits', '']);
-(function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4416148&101" alt="" border="0"></a></noscript>
-<!-- Histats.com  END  -->
-
-<--ads/responsive-->	</main>
-</body>
-</html>
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
